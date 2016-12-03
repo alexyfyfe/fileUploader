cb1829d08d724fc146310114c5fe2e411f168a01
alexyfyfe <alexyfyfe@hotmail.com>
2016-12-03 18:16:18 +0000

    xgfds

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,18 @@
-?<php
-    $dbhost = "eu-cdbr-azure-north-e.cloudapp.net";
-    $dbuser = "bcfb17955d26fa";
-    $dbpass = "f5e7a502";
-    $dbname = "database_ajafyfe";
-    mysql_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server');
-mysql_select_db($dbname) or die('database selection problem');
+
+<?php
+/*
+ * Connect to your Azure server and select database
+ * Your connection details are all on the azure portal
+ */
+$db = new mysqli(
+    "eu-cdbr-azure-north-e.cloudapp.net", //Server Name
+    "b1fa144aa688ff", //Username
+    "4e96e436",
+    "db_pgo_cs7" ); //Database
+/*
+ * If database fails to connect - error message displays
+ */
+if ($db->connect_errno){
+    die ('Connection Failed :'.$db->connect_error);
+}
 ?>
