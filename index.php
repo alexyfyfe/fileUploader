42c6c9279e3e4455c5e3bb548c12f736734d3f73
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-01 17:48:18 +0000

    Test

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 $arr = array('one','two','three','four','stop','five');
 $count = 0;
 
+while (list(, $val) = each($arr)){
+    $arr[$count] = 'hi';
+    $count++;
+}
 
 while (list(, $val) = each($arr)){
-    echo "hi";
     echo "$val<br />\n";
 }
 
