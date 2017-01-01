f347f541072d6369e3a0b2849ecb7ad11120bdd7
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-01 17:53:31 +0000

    Test

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ $arr = array('one','two','three','four','stop','five');
 
 foreach ($arr as $day){
     $day = 'hi';
-}
+};
 
 while (list(, $val) = each($arr)){
     echo "$val<br />\n";
-}
+};
 
 ?>
