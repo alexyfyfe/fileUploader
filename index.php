04f8fd1d45677020ce8ccb629ac5e544a2773351
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-01 17:46:50 +0000

    Test

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 $arr = array('one','two','three','four','stop','five');
 $count = 0;
 
-while (list(, $val) = each($arr)){
-    $arr[$count] = substr($val,0,3);
-    $count++;
-}
 
 while (list(, $val) = each($arr)){
     echo "hi";
