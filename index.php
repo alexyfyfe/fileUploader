9a006fa30c79b1ab9b656997e9f6cc7567ac7b52
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-03 21:13:09 +0000

    Test

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ $cent = 10;
 
 static $coins_of_usa = array(1=>"penny", 5=>"nickel", 10=>"dime", 25=>"quarter", 50=>"half dollar", 100=>"dollar");
 
-foreach ($coins_of_usa as $coins){
-    echo "$coins";
+foreach ($coins_of_usa as $x => $x_value){
+    if($cent == $x){
+        echo "$cent = $x_value";
+    }
 }
 
 
