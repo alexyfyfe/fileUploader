729ae1cb3ce66c28d5188f443c9ebe399bc1593b
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-03 21:10:51 +0000

    Test

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
 
-$x = 1;
-if ($x == 1.0){
-    echo "True";
-}else{
-    echo "False";
+$cent = 10;
+
+static $coins_of_usa = array(1=>"penny", 5=>"nickel", 10=>"dime", 25=>"quarter", 50=>"half dollar", 100=>"dollar");
+
+foreach ($coins_of_usa as $coins){
+    echo "$coins";
 }
-$x = 1.0;
-if ($x === 1){
-    echo "True";
-}else{
-    echo "False";
-}
+
 
 ?>
