61c819d9aae6fc27cf873e353d46bdd0e367b242
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-03 21:41:08 +0000

    Test

--- index.php
+++ index.php
@@ -5,16 +5,15 @@ $payroll = array(
     456=>array("jane smythe", 12000.0),
     789=>array("jeremy smithers", 14000.0)
 );
-$payroll = payrise($payroll, 10.0);
+payrise($payroll, 10.0);
 var_dump($payroll);
 
-function payrise ($payroll, $rise){
-    foreach ($payroll as $id => $person){
+function payrise (&$payroll, $rise){
+    foreach ($payroll as $id => &$person){
         foreach ($person as $name => $pay){
             $person[$name] = $pay*($rise/100+1);
         }
     }
-    return $payroll;
 }
 
 ?>
