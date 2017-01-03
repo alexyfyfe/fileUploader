0270b498e9f6198b7eb74b59ba08dc48b58f344a
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-03 21:28:39 +0000

    Test

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 $payroll = array(
     123=>array("john smith", 10000.0),
-    456=>array("jane smythe", 12000.0)
+    456=>array("jane smythe", 12000.0),
+    789=>array("jeremy smithers", 14000.0)
 );
 payrise($payroll, 10.0);
 var_dump($payroll);
@@ -10,7 +11,8 @@ var_dump($payroll);
 function payrise ($payroll, $rise){
     foreach ($payroll as $id => $person){
         foreach ($person as $name => $pay){
-            $pay = $pay*($rise/100+1);
+            $updatePay = $pay*($rise/100+1);
+            echo "$updatePay";
         }
     }
 }
