f22b52d222e58013ec6b742dc42a574de587fd29
alexyfyfe <alexyfyfe@hotmail.com>
2017-01-03 20:52:13 +0000

    Test

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
-<html>
-<head>
-    <title>Count Page Access</title>
-</head>
-<body>
 <?php
 
-if (!isset($_COOKIE['count']))
-{
-    ?>
-    Welcome! This is the first time you have viewed this page.
-    <?php
-    $cookie = 1;
-    setcookie("count", $cookie);
+$x = 1;
+if ($x == 1.0){
+    echo "True";
+}else{
+    echo "False";
 }
-else
-{
-    $cookie = ++$_COOKIE['count'];
-    setcookie("count", $cookie);
-    ?>
-    You have viewed this page <?= $_COOKIE['count'] ?> times.
-<?php  }// end else  ?>
-</body>
-</html>
+$x = 1.0;
+if ($x === 1){
+    echo "True";
+}else{
+    echo "False";
+}
+
+?>
